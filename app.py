aadd0691dc24bbfc79630d55788be5b6cc4e1141
kalyan <145825506+kalyansale755@users.noreply.github.com>
2026-06-02 13:10:45 +0530

    code changed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-##server_loc = st.secrets["SERVER_URL"]
+server_loc = st.secrets["SERVER_URL"]
 
 with st.form("details"):
     st.title("AI INTERVIEW QUESTIONS CHAT BOT ")
